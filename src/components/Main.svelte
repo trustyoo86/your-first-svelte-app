2a1077059eb0ad58ce1bfc87710ee14ccb7d9b16
kern.yoo <trustyoo86@gmail.com>
2020-02-12 04:49:50 +0900

    Add svelte component

---
+++ src/components/Main.svelte
@@ -0,0 +1,15 @@
+<script lang="typescript">
+  export let test = 'mistery';
+  const action: string = 'Click!';
+  
+  function handleClick(): void {
+    alert('click!!');
+  }
+</script>
+
+<div>
+  <button on:click={handleClick}>{test} {action}</button>
+</div>
+
+<style>
+</style>
